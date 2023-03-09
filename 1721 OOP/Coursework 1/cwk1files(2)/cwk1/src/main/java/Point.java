11993ac163e3191f2fb357a131d1a1d80991d59f
sc22j4s <117829431+sc22j4s@users.noreply.github.com>
2023-03-09 00:17:28 +0000

    HEKLP PLEASE

--- 1721 OOP/Coursework 1/cwk1files(2)/cwk1/src/main/java/Point.java
+++ 1721 OOP/Coursework 1/cwk1files(2)/cwk1/src/main/java/Point.java
@@ -7,7 +7,6 @@ import static java.lang.Math.sin;
 import static java.lang.Math.sqrt;
 import static java.lang.Math.toRadians;
 
-
 /**
  * Represents a point in space and time, recorded by a GPS sensor.
  *
@@ -22,39 +21,64 @@ public class Point {
   private static final double MAX_LATITUDE = 90.0;
   private static final double MEAN_EARTH_RADIUS = 6.371009e+6;
 
-  private static double longitude;
-  private static double latitude; 
-  private static double elevation;
-  
-  public Point(){
-    
-  }
+  // point fields initialised
+  private ZonedDateTime timestamp;
+  private double longitude;
+  private double latitude;
+  private double elevation;
 
   // TODO: Create a stub for the constructor
 
+  public Point(ZonedDateTime t, double lon, double lat, double elev) {
+
+    timestamp = t;
+
+    // validate longitude/latitude against constants
+    if (lon >= MIN_LONGITUDE && lon <= MAX_LONGITUDE) {
+      longitude = lon;
+    } else {
+      throw new GPSException(null);
+    }
+    if (lat >= MIN_LATITUDE && lat <= MAX_LATITUDE) {
+      latitude = lat;
+    } else {
+      throw new GPSException(null);
+    }
+
+    elevation = elev;
+  }
+
   // TODO: Create a stub for getTime()
 
-  public 
+  public ZonedDateTime getTime() {
+    return this.timestamp;
+  }
 
   // TODO: Create a stub for getLatitude()
 
-  public double getLatitude(){
-    return latitude;
+  public double getLatitude() {
+    return this.latitude;
   }
 
   // TODO: Create a stub for getLongitude()
 
-  public double getLongitude(){
-    return longitude;
+  public double getLongitude() {
+    return this.longitude;
   }
 
-  // TODO: Create a stub for getElevation()
+  // TODO: Create a stub for getElevation() (done)
 
-  public double getElevation(){
-    return elevation;
+  public double getElevation() {
+    return this.elevation;
   }
 
-  // TODO: Create a stub for toString()
+  // TODO: Create a stub for toString() (done)
+
+  public String toString() {
+    // format to 5 decimal places for long/lat, 1 for elev
+    String s = String.format("(%.5f, %.5f), %.1f m", this.longitude, this.latitude, this.elevation);
+    return s;
+  }
 
   // IMPORTANT: Do not alter anything beneath this comment!
 
@@ -74,11 +98,11 @@ public class Point {
     double lambda2 = toRadians(q.getLongitude());
     double delta = abs(lambda1 - lambda2);
 
-    double firstTerm = cos(phi2)*sin(delta);
-    double secondTerm = cos(phi1)*sin(phi2) - sin(phi1)*cos(phi2)*cos(delta);
-    double top = sqrt(firstTerm*firstTerm + secondTerm*secondTerm);
+    double firstTerm = cos(phi2) * sin(delta);
+    double secondTerm = cos(phi1) * sin(phi2) - sin(phi1) * cos(phi2) * cos(delta);
+    double top = sqrt(firstTerm * firstTerm + secondTerm * secondTerm);
 
-    double bottom = sin(phi1)*sin(phi2) + cos(phi1)*cos(phi2)*cos(delta);
+    double bottom = sin(phi1) * sin(phi2) + cos(phi1) * cos(phi2) * cos(delta);
 
     return MEAN_EARTH_RADIUS * atan2(top, bottom);
   }
---
+++ 1721 OOP/test/main.java
@@ -0,0 +1,7 @@
+
+class main {
+    public static void main(String args[]){
+        double g = 3.52345;
+        System.out.printf("%.5f", g);
+    }
+}
